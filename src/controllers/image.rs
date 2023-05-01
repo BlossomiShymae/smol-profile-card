e80d05a161fe8bcc0d53ba6a6fa91d8faccdaa4b
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-30 22:13:11 -0500

    Add scuffed database save and read for user

--- src/controllers/image.rs
+++ src/controllers/image.rs
@@ -3,6 +3,7 @@ use axum::http::StatusCode;
 use axum::extract::{State, Query};
 use axum::response::Html;
 use serde::Deserialize;
+use rusqlite::params;
 
 use crate::AppState;
 use crate::models::empty::{Empty};
@@ -14,35 +15,91 @@ pub struct GithubUserViewModel {
 
 pub async fn get_index(query: Query<GithubUserViewModel>, State(state): State<Arc<AppState>>) -> (StatusCode, Html<String>) {
     let vm = query.0;
-    let url = format!("https://api.github.com/users/{}", vm.user.as_str());
-    log::info!("{}", url);
+    let username_move = vm.user.to_string();
+    let username = vm.user.to_string();
 
-    let response = match state.client.get(url)
-        .header("User-Agent", "BlossomiShymae/smol-profile-card")
-        .header("Accept", "application/json")
-        .send()
-        .await {
-            Ok(r) => r,
-            Err(_) => panic!()
-        };
-    if !response.status().is_success() {
-        log::error!("{:?}", response.status());
-        log::error!("{:?}", response.text().await.unwrap());
-        let data = super::TemplateViewModel {
-            title: "500 Internal Server Error".into(),
-            body: state.registry.render("errors/500", &Empty).unwrap(),
-        };
+    // Attempt to use database cache before making request
+    let user_option = state.conn.call(move |conn| {
+        let query = format!("SELECT * FROM GitHubUser WHERE username = '{}'", username_move);
+        let mut stmt = conn.prepare(query.as_str()).unwrap();
+        let users = stmt.query_map([], |row| {
+            Ok(crate::entities::github_user::GithubUser {
+                id: row.get(0)?,
+                username: row.get(1)?,
+                name: row.get(2)?,
+                location: row.get(3)?,
+                avatar_url: row.get(4)?,
+            })
+        })?.collect::<Result<Vec<crate::entities::github_user::GithubUser>, rusqlite::Error>>()?;
 
-        let r = state.registry.render("template", &data).unwrap();
-        return (StatusCode::INTERNAL_SERVER_ERROR, Html(r));
-    }
-    
-    let contents = match response.text().await {
-        Ok(s) => s,
-        Err(_) => panic!()
+        Ok::<_, rusqlite::Error>(users)
+    })
+    .await.unwrap().first().cloned();
+
+    // Return a user based on option
+    let user = match user_option {
+        Some(user) => {
+            log::info!("Hit for GitHub user with username: {}!", user.username);
+            user.clone()
+        },
+        None => {
+            log::info!("Miss for GitHub user with username: {}!", username);
+            let url = format!("https://api.github.com/users/{}", username);
+            log::info!("{}", url);
+
+            let response = match state.client.get(url)
+                .header("User-Agent", "BlossomiShymae/smol-profile-card")
+                .header("Accept", "application/json")
+                .send()
+                .await {
+                    Ok(r) => r,
+                    Err(_) => panic!()
+                };
+            if !response.status().is_success() {
+                log::error!("{:?}", response.status());
+                log::error!("{:?}", response.text().await.unwrap());
+                let data = super::TemplateViewModel {
+                    title: "500 Internal Server Error".into(),
+                    body: state.registry.render("errors/500", &Empty).unwrap(),
+                };
+
+                let r = state.registry.render("template", &data).unwrap();
+                return (StatusCode::INTERNAL_SERVER_ERROR, Html(r));
+            }
+            
+            let contents = match response.text().await {
+                Ok(s) => s,
+                Err(_) => panic!()
+            };
+
+            let user: crate::models::github_user::GithubUser = serde_json::from_str(&contents).unwrap();
+            let entity = crate::entities::github_user::GithubUser {
+                id: user.id,
+                username: user.login,
+                name: user.name,
+                avatar_url: user.avatar_url,
+                location: user.location
+            };
+
+            let s_entity = entity.clone();
+            // Save entity to database cache
+            state.conn.call(move |conn| {
+                let query = "INSERT INTO GitHubUser (id, username, name, location, avatar_url) VALUES (?1, ?2, ?3, ?4, ?5)";
+                conn.execute(query, params![
+                    s_entity.id, 
+                    s_entity.username, 
+                    s_entity.name, 
+                    s_entity.location, 
+                    s_entity.avatar_url
+                    ]
+                ).unwrap();
+
+                Ok(())
+            }).await.unwrap();
+
+            entity
+        },
     };
-
-    let user: crate::models::github_user::GithubUser = serde_json::from_str(&contents).unwrap();
 
     (StatusCode::OK, Html(user.location.into()))
 }
--- src/entities/github_user.rs
+++ src/entities/github_user.rs
@@ -1,5 +1,5 @@
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct GithubUser {
     pub id: i32,
     pub username: String,
