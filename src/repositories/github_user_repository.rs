559842604c3929c4f8434c98cd80500c73c0d23c
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-02 09:42:42 -0500

    Add get by ID method

--- src/repositories/github_user_repository.rs
+++ src/repositories/github_user_repository.rs
@@ -28,6 +28,24 @@ impl GitHubUserRepository {
         }).await.unwrap().first().cloned()
     }
 
+    pub async fn get_by_id(&self, id: i32) -> Option<GithubUser> {
+        self.conn.call(move |conn| {
+            let query = format!("SELECT * FROM GitHubUser WHERE id = {}", id);
+            let mut stmt = conn.prepare(query.as_str()).unwrap();
+            let users = stmt.query_map([], |row| {
+                Ok(crate::entities::github_user::GithubUser {
+                    id: row.get(0)?,
+                    username: row.get(1)?,
+                    name: row.get(2)?,
+                    location: row.get(3)?,
+                    avatar_url: row.get(4)?,
+                })
+            })?.collect::<Result<Vec<crate::entities::github_user::GithubUser>, rusqlite::Error>>()?;
+
+            Ok::<_, rusqlite::Error>(users)
+        }).await.unwrap().first().cloned()
+    }
+
     pub async fn upsert(&self, entity: GithubUser) -> Result<(), tokio_rusqlite::Error> {
         let insert_result = self.insert(entity.clone()).await;
         if let Ok(()) = insert_result {
