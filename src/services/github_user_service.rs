f3051d11fee2c28605b87a5b123d4904f626ab6f
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-02 10:40:21 -0500

    Use expiration caching for user

--- src/services/github_user_service.rs
+++ src/services/github_user_service.rs
@@ -17,41 +17,50 @@ impl GitHubUserService {
         let stored_user_option = self.repository.get_by_username(username).await;
         let result_option: Result<Option<GithubUser>, Box<dyn Error + Send + Sync>> = match stored_user_option {
             Some(user) => {
+                // Check if user in database cache is expired
+                let current_timestamp = chrono::prelude::Utc::now().timestamp_millis();
+                if current_timestamp >= user.expiration {
+                    // Miss
+                    return self.update_user(username).await;
+                }
+                // Hit
                 log::info!("Hit for GitHub user, username: {}!", username_clone);
                 Ok(Some(github_user_mapper::to_model(&user)))
             },
-            None => {
-                log::info!("Miss for GitHub user, username: {}!", username_clone);
-                let url = format!("https://api.github.com/users/{}", username_clone);
-                log::info!("Making request to {}...", url);
-
-                let response = self.client.get(url)
-                    .header("User-Agent", "BlossomiShymae/smol-profile-card")
-                    .header("Accept", "application/json")
-                    .send()
-                    .await
-                    .expect("Failed to get response");
-
-                if !response.status().is_success() {
-                    log::error!("{:?}", response.status());
-                    log::error!("{:?}", response.text().await.unwrap());
-                    return Err(String::from("Failed to get response!"))?;
-                }
-
-                let contents = response.text().await.expect("Failed to get response!");
-                let user: GithubUser = serde_json::from_str(&contents).expect("Failed to deserialize GithubUser");
-                let upsert_result = self.repository.upsert(github_user_mapper::to_entity(&user)).await;
-
-                match upsert_result {
-                    Ok(()) => Ok(Some(user)),
-                    Err(e) => {
-                        log::error!("Failed to upsert user: {}", user.login);
-                        log::error!("{:?}", e);
-                        return Err(String::from("Failed to upsert user!"))?;
-                    }
-                }
-            }
+            None => self.update_user(username).await
         };
         result_option
+    }
+
+    async fn update_user(&self, username: &str) -> Result<Option<GithubUser>, Box<dyn Error + Send + Sync>> {
+        log::info!("Miss for GitHub user, username: {}!", username);
+        let url = format!("https://api.github.com/users/{}", username);
+        log::info!("Making request to {}...", url);
+
+        let response = self.client.get(url)
+            .header("User-Agent", "BlossomiShymae/smol-profile-card")
+            .header("Accept", "application/json")
+            .send()
+            .await
+            .expect("Failed to get response");
+
+        if !response.status().is_success() {
+            log::error!("{:?}", response.status());
+            log::error!("{:?}", response.text().await.unwrap());
+            return Err(String::from("Failed to get response!"))?;
+        }
+
+        let contents = response.text().await.expect("Failed to get response!");
+        let user: GithubUser = serde_json::from_str(&contents).expect("Failed to deserialize GithubUser");
+        let upsert_result = self.repository.upsert(github_user_mapper::to_entity(&user)).await;
+
+        match upsert_result {
+            Ok(()) => Ok(Some(user)),
+            Err(e) => {
+                log::error!("Failed to upsert user: {}", user.login);
+                log::error!("{:?}", e);
+                return Err(String::from("Failed to upsert user!"))?;
+            }
+        }
     }
 }
