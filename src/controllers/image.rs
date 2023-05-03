e26503ceb332702062b0b81a97155b0f6d5084b4
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-03 10:12:26 -0500

    Use default values for name and location

--- src/controllers/image.rs
+++ src/controllers/image.rs
@@ -5,7 +5,7 @@ use axum::http::{StatusCode};
 use axum::extract::{State, Query};
 use axum::response::{Response, IntoResponse};
 use image::imageops::FilterType;
-use image::{Rgba, ImageFormat, DynamicImage, GenericImageView, RgbaImage};
+use image::{Rgba, ImageFormat, DynamicImage, RgbaImage, ImageBuffer};
 use serde::Deserialize;
 use rusttype::{Scale, Font};
 
@@ -64,39 +64,21 @@ pub async fn get_index(query: Query<GithubUserViewModel>, State(state): State<Ar
             if avatar_result.is_err() {
                 return super::get_error_page(&state.registry, StatusCode::INTERNAL_SERVER_ERROR).await
             }
+
+            // Load avatar image
             let avatar = avatar_result.unwrap();
             let mut avatar_img = image::load_from_memory(&avatar).unwrap();
-            let mut img = draw_image(&user, &pronouns_tag).await;
-
             // Border round the avatar
             let mut canvas_avatar = avatar_img.to_rgba8();
-            let dim = avatar_img.dimensions();
-            let mut canvas_mask = RgbaImage::new(dim.0, dim.1);
-            let width = dim.0 as f32;
-            let height = dim.1 as f32;
-            let midpoint = ((width/2.0) as i32, (height/2.0) as i32);
-            let radius = (width as i32) - midpoint.0;
-            imageproc::drawing::draw_filled_circle_mut(
-                &mut canvas_mask, 
-                midpoint,
-                radius,
-                Rgba([255,255,255,255])
-            );
-            // Apply clip mask
-            for (avatar_p, mask_p) in canvas_avatar.pixels_mut().zip(canvas_mask.pixels_mut()) {
-                if mask_p.0 != [255,255,255,255] {
-                    avatar_p.0 = [0,0,0,0];
-                }
-            }
-            
+            round_image_mut(&mut canvas_avatar);
             // Overlay avatar onto image
             avatar_img = canvas_avatar.into();
             avatar_img = avatar_img.resize(100, 100, FilterType::Lanczos3);
-            image::imageops::overlay(&mut img, &avatar_img, 20, 10);
-            
+            let mut card_img = draw_image(&user, &pronouns_tag).await;
+            image::imageops::overlay(&mut card_img, &avatar_img, 20, 10);
             // Serialize image
             let mut buffer = BufWriter::new(Cursor::new(Vec::new()));
-            img.write_to(&mut buffer, ImageFormat::Png).unwrap();
+            card_img.write_to(&mut buffer, ImageFormat::Png).unwrap();
             let bytes: Vec<u8> = buffer.into_inner().unwrap().into_inner();
             
             return Response::builder()
@@ -112,6 +94,27 @@ pub async fn get_index(query: Query<GithubUserViewModel>, State(state): State<Ar
     super::get_error_page(&state.registry, StatusCode::INTERNAL_SERVER_ERROR).await
 }
 
+fn round_image_mut(image: &mut ImageBuffer<Rgba<u8>, Vec<u8>>) {
+    let dim = image.dimensions();
+    let mut canvas_mask = RgbaImage::new(dim.0, dim.1);
+    let width = dim.0 as f32;
+    let height = dim.1 as f32;
+    let midpoint = ((width/2.0) as i32, (height/2.0) as i32);
+    let radius = (width as i32) - midpoint.0;
+    imageproc::drawing::draw_filled_circle_mut(
+        &mut canvas_mask, 
+        midpoint,
+        radius,
+        Rgba([255,255,255,255])
+    );
+    // Apply clip mask
+    for (avatar_p, mask_p) in image.pixels_mut().zip(canvas_mask.pixels_mut()) {
+        if mask_p.0 != [255,255,255,255] {
+            avatar_p.0 = [0,0,0,0];
+        }
+    }
+}
+
 async fn draw_image(user: &crate::models::github_user::GithubUser, pronouns_tag: &str) -> DynamicImage {
     // Create profile card image
     let mut img = image::open("images/dark_template.png").unwrap();
@@ -124,49 +127,52 @@ async fn draw_image(user: &crate::models::github_user::GithubUser, pronouns_tag:
     let smol_font_size = 20.0;
     
     // Draw the person's name
-    let left_margin = 140;
-    imageproc::drawing::draw_text_mut(
-        &mut img, 
-        Rgba([255u8, 255u8, 255u8, 255u8]), 
-        left_margin, 
-        20, 
-        Scale { x: big_font_size, y: big_font_size },
-        &regular_font, 
-        &user.name
-    );
-
-    // Draw the person's location
-    imageproc::drawing::draw_text_mut(
-        &mut img, 
-        Rgba([192u8, 192u8, 192u8, 255u8]), 
-        left_margin + 20, 
-        50, 
-        Scale {
-            x: smol_font_size,
-            y: smol_font_size
-        }, 
-        &light_font,
-        &user.location
-    );
-
-    // Overlay location icon
-    image::imageops::invert(&mut location_img);
-    let buffer = image::imageops::brighten(&location_img, -25);
-    image::imageops::overlay(&mut img, &buffer, i64::from(left_margin), 55);
-
-    // Draw the person's pronouns
-    imageproc::drawing::draw_text_mut(
-        &mut img, 
-        Rgba([192u8, 192u8, 192u8, 255u8]), 
-        left_margin, 
-        78, 
-        Scale {
-            x: smol_font_size,
-            y: smol_font_size
-        }, 
-        &light_font, 
-        pronouns_tag.clone()
-    );
+    {
+        let left_margin = 140;
+        let user = user.clone();
+        imageproc::drawing::draw_text_mut(
+            &mut img, 
+            Rgba([255u8, 255u8, 255u8, 255u8]), 
+            left_margin, 
+            20, 
+            Scale { x: big_font_size, y: big_font_size },
+            &regular_font, 
+            &user.name.unwrap_or(user.login.to_string())
+        );
+        // Draw the person's location
+        let location = &user.location.unwrap_or("".to_string());
+        imageproc::drawing::draw_text_mut(
+            &mut img, 
+            Rgba([192u8, 192u8, 192u8, 255u8]), 
+            left_margin + 20, 
+            50, 
+            Scale {
+                x: smol_font_size,
+                y: smol_font_size
+            }, 
+            &light_font,
+            location
+        );
+        // Overlay location icon
+        if location.ne("") {
+            image::imageops::invert(&mut location_img);
+            let buffer = image::imageops::brighten(&location_img, -25);
+            image::imageops::overlay(&mut img, &buffer, i64::from(left_margin), 55);
+        }
+        // Draw the person's pronouns
+        imageproc::drawing::draw_text_mut(
+            &mut img, 
+            Rgba([192u8, 192u8, 192u8, 255u8]), 
+            left_margin, 
+            78, 
+            Scale {
+                x: smol_font_size,
+                y: smol_font_size
+            }, 
+            &light_font, 
+            pronouns_tag.clone()
+        );
+    };   
 
     img
 }
