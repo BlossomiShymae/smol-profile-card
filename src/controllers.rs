ac52db947cb839bb54021582e5724a43bc44e09b
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-03 00:45:13 -0500

    Use error template for get_error_page

--- src/controllers.rs
+++ src/controllers.rs
@@ -6,8 +6,6 @@ use reqwest::StatusCode;
 use serde::{Serialize};
 use handlebars::Handlebars;
 
-use crate::models::empty::Empty;
-
 
 #[derive(Debug, Serialize)]
 pub struct TemplateViewModel {
@@ -15,16 +13,31 @@ pub struct TemplateViewModel {
     pub body: String,
 }
 
+#[derive(Debug, Serialize)]
+pub struct TemplateErrorViewModel {
+    pub code: u16,
+    pub reason: String,
+}
+
 pub async fn get_error_page(registry: &Handlebars<'static>, status_code: StatusCode) -> (StatusCode, Html<String>) {
-    let data_tuple: (&str, &str) = match status_code {
-        StatusCode::INTERNAL_SERVER_ERROR => ("500 Internal Server Error", "errors/500"),
-        _ => panic!("Status code not implemented")
+    let status_title = get_status_title(status_code);
+    let template_error_vm = TemplateErrorViewModel {
+        code: status_code.as_u16(),
+        reason: status_code.canonical_reason().unwrap_or_else(|| {""}).to_string()
     };
+    let error_r = registry.render("errors/template", &template_error_vm).unwrap();
+
     let template_vm = TemplateViewModel {
-        title: data_tuple.0.into(),
-        body: registry.render(data_tuple.1.into(), &Empty).unwrap(),
+        title: status_title,
+        body: error_r,
     };
     let r = registry.render("template", &template_vm).unwrap();
     
     (status_code, Html(r))
+}
+
+fn get_status_title(status_code: StatusCode) -> String {
+    let code = status_code.as_u16().to_string();
+    let reason = status_code.canonical_reason().unwrap_or_else(|| {""}).to_string();
+    [code, reason].join(" ")
 }
